1dfb4392d048c1204b9d70d18abb0ccb043c8e83
nodarisvanidze88 <nodarisvanidze88@gmail.com>
2023-06-17 10:38:41 +0400

    Create first 2 excersises of 57 problems

---
+++ Ex_1_Saying_Hello/Saying_Hello_v1.py
@@ -0,0 +1,5 @@
+def main():
+    print(f'Hello, {input("What is your name? ")}, nice to meet you!')
+
+if __name__=="__main__":
+    main()
---
+++ Ex_2_Counting_Number_of_Characters/Counting_numbers.py
@@ -0,0 +1,6 @@
+def main():
+    string =input("What is the input string? ")
+    print(f"{string} has {len(string)} characters.")
+
+if __name__=="__main__":
+    main()
